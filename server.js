46605dc2d85e793eaa576f8df5054139d8123465
Rhino Lee <picklerhino@gmail.com>
2021-06-14 01:02:27 +0800

    boardcast

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || 3000;
 const express = require('express')
 const INDEX = '/index.html';
 const { Server } = require('ws');
-let userCount = [];
+let userCount = 0;
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
@@ -12,13 +12,19 @@ const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('server connection')
-  server.send("userCount " + ++userCount);
-  
-  ws.addListener("message", function(message){
-    server.send(message);
+  userCount += 1
+
+  ws.on('message', function incoming(data) {
+    console.log('received: %s', data);
+    wss.clients.forEach(function each(client) {
+      client.send(data);
+    });
   });
 
+  ws.send(`userCount-${userCount}`);
+
   ws.on('close', () => {
+    userCount -= 1
     ws.send("ws close");
   })
 });
